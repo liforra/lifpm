48d73ff2ed277346fe4a6550a2ee69574f27b7c4
liforra <github@liforra.de>
2025-02-11 15:05:29 +0100

    Add pacman apt & .gitignore

--- install.py
+++ install.py
@@ -1,3 +1,12 @@
 import os
-def pacman(package, root:bool):
-    print("UwU")
+def pacman(package:str, root:bool):
+    if root:
+        os.system(f'pacman -S {package}')
+    else:
+        os.system(f'sudo pacman -S {package}')
+
+def apt(packman:str, root:bool):
+    if root:
+        os.system(f'apt install {package}')
+    else:
+        os.system(f'sudo apt install {package}')
