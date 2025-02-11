2fb6a9275683e743bae6ea50d2f35079279db9b7
liforra <github@liforra.de>
2025-02-11 13:55:45 +0100

    Detect Package Managers

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 try:
     import sys
+    import shutil
 except ImportError:
     raise ImportError("Missing Modules, please try installing all dependencies")
 def linux():
     return 0
 def windows():
     raise OSError("Unfortunetly Windows is not yet Supported.")
+
+def detect_pms():
+    pms = ["pacman", "apt", "flatpak", "paru", "yay", "snap", "nix", "apk", "dnf", "yum"]
+    installedpms = []
+    for pm in pms:
+        if shutil.which(pm):
+            installedpms.append(pm)
+    return installedpms
+
 if sys.platform == "win32":
     windows()
 elif sys.platform == "linux":
     linux()
 else:
     raise(OSError("Unsupported Operating System: " + sys.platform))
-
-
-
+installedpms = detect_pms()
 
